b3c19512521bbc90e8fcaf2f6456eaa49e11598b
Ken Krippeler <kriken19@gmail.com>
2018-03-25 21:06:59 +0200

    Persistent data using Codable protocol and plist

--- Checky/Controller/ToDoListViewController.swift
+++ Checky/Controller/ToDoListViewController.swift
@@ -12,30 +12,12 @@ class ToDoListViewController: UITableViewController
 {
     var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
-        let newItem = Item()
-        newItem.title = "Learn Swift"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Play a game"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Watch a video"
-        itemArray.append(newItem3)
-        
-        if let items = defaults.array(forKey: "ToDoListArray") as? [Item]
-        {
-            itemArray = items
-        }
-        
-        tableView.reloadData()
+        loadItems()
     }
 
     //MARK - TableViewDataSource Methods
@@ -60,11 +42,8 @@ class ToDoListViewController: UITableViewController
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        //print(itemArray[indexPath.row])
-        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        
-        tableView.reloadData()
+        saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
@@ -86,8 +65,7 @@ class ToDoListViewController: UITableViewController
                 newItem.title = item
                 
                 self.itemArray.append(newItem)
-                self.defaults.set(self.itemArray, forKey: "ToDoListArray")
-                self.tableView.reloadData()
+                self.saveItems()
             }
         }
         
@@ -113,7 +91,42 @@ class ToDoListViewController: UITableViewController
         present(alert, animated: true, completion: nil)
     }
     
+    //MARK - Model Manipulation Methods
     
+    func saveItems()
+    {
+        let encoder = PropertyListEncoder()
+        
+        do
+        {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        }
+        catch
+        {
+            print("Error encoding item array, \(error.localizedDescription)")
+        }
+        
+        self.tableView.reloadData()
+    }
     
+    func loadItems()
+    {
+        if let data = try? Data(contentsOf: dataFilePath!)
+        {
+            let decoder = PropertyListDecoder()
+            
+            do
+            {
+                itemArray = try decoder.decode([Item].self, from: data)
+            }
+            catch
+            {
+                print("Error decoding item array, \(error.localizedDescription)")
+            }
+        }
+        
+        self.tableView.reloadData()
+    }
 }
 
